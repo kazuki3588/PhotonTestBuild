1149210f5d132215ef822b13d7f4397dfdbca93d
kazuki3588 <kuramoto880@ezweb.ne.jp>
2021-08-25 14:33:28 +0900

    PlayerUI実装

--- Build/PhotonTestBuild.framework.js
+++ Build/PhotonTestBuild.framework.js
@@ -1308,7 +1308,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2925232;
+STATICTOP = STATIC_BASE + 2925504;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AIScriptingClasses_cpp();
@@ -3286,7 +3286,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2925232;
+var STATIC_BUMP = 2925504;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
